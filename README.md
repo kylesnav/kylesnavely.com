d79d6d43906ebc36e50767627802447b9973cac0
Kyle Snavely <me@kylesnavely.com>
2023-05-08 12:28:17 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# kylesnavely.com
+This is–or will be–the source code for my personal website, [kylesnavely.com](https://kylesnavely.com). It is built using Python, Flask, and Bootstrap for CSS.
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("pages/index.html", title="Home")
+
+@app.route("/links")
+def links():
+    return render_template("pages/links.html", title="Links")
+
+@app.route("/resume")
+def resume():
+    return render_template("pages/resume.html", title="Resume")
+
+@app.route("/about")
+def about():
+    return render_template("pages/about.html", title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template("pages/contact.html", title="Contact")
+
+if __name__ == "__main__":
+    app.run(debug=True)
